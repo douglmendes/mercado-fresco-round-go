07404c7f5d11d21724bdb5af83f456b6b26b520a
Renata Leal <renata.leal@mercadolivre.com>
2022-06-04 20:55:26 -0300

    feature sellers first commit

--- cmd/server/controllers/sellers.go
+++ cmd/server/controllers/sellers.go
@@ -1,1 +1,148 @@
 package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/douglmendes/mercado-fresco-round-go/internal/sellers"
+	"github.com/douglmendes/mercado-fresco-round-go/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+type SellerController struct {
+	service sellers.Service
+}
+
+type request struct {
+	Cid         int `json:"cid" binding:"required"`
+	CompanyName string `json:"company_name" binding:"required"`
+	Address     string `json:"address" binding:"required"`
+	Telephone   string `json:"telephone" binding:"required"`
+}
+
+func NewSeller(s sellers.Service) *SellerController {
+	return &SellerController{
+		service: s,
+	}
+
+}
+
+// ListSellers godoc
+// @Summary List sellers
+// @Tags Sellers
+// @Description get sellers
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} request
+// @Router /sellers [get]
+func (c *SellerController) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		s, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(404, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, response.NewResponse(http.StatusOK, s))
+	}
+}
+
+func (c *SellerController) GetById() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+
+		s, err := c.service.GetById(int(id))
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, response.NewResponse(http.StatusOK, s))
+	}
+}
+
+// StoreProducts godoc
+// @Summary Store sellers
+// @Tags Sellers
+// @Description store sellers
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body request true "Seller to store"
+// @Success 200 {object} web.Response
+// @Failure 400 {object} web.Response
+// @Failure 401 {object} web.Response
+// @Router /sellers [post]
+func (c *SellerController) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				gin.H{
+					"error":   "VALIDATEERR-1",
+					"message": "Invalid inputs. Please check your inputs",
+				})
+			return
+		}
+
+		s, err := c.service.Store(req.Cid, req.CompanyName, req.Address, req.Telephone)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, response.NewResponse(http.StatusOK, s))
+	}
+
+}
+
+func (s *SellerController) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		s, err := s.service.Update(int(id), req.Cid, req.CompanyName, req.Address, req.Telephone)
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(200, s)
+	}
+	
+}
+
+func (c *SellerController) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+
+		err = c.service.Delete(int(id))
+		if err != nil {
+			ctx.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(200, gin.H{"data": fmt.Sprintf("o vendedor %d foi removido", id)})
+	}
+	
+}
