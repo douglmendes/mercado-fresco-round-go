5febcf3f2dba8b61352a9bc7dcfdaeb9e52af405
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 15:23:46 -0300

    add GetAll function to service

--- internal/products/service.go
+++ internal/products/service.go
@@ -1,1 +1,22 @@
 package products
+
+type Service interface {
+	GetAll() ([]Product, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{repository: r}
+}
+
+func (s service) GetAll() ([]Product, error) {
+	products, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
