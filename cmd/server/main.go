d77a8e4d69669bb2d9d8b63faed267bf200628fa
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 17:43:29 -0300

    add GetById product route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -73,6 +73,7 @@ func main() {
 	productsRoutes := router.Group("/api/v1/products")
 	{
 		productsRoutes.GET("/", productsController.GetAll())
+		productsRoutes.GET("/:id", productsController.GetById())
 		productsRoutes.POST("/", productsController.Store())
 	}
 
