e983d8253d461e35aac1a66c117d6211d00f8f1f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 15:36:55 -0300

    add GetAll function to controller

--- cmd/server/controllers/products.go
+++ cmd/server/controllers/products.go
@@ -1,1 +1,32 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/douglmendes/mercado-fresco-round-go/internal/products"
+	"github.com/gin-gonic/gin"
+)
+
+type ProductController struct {
+	service products.Service
+}
+
+func NewProductController(service products.Service) *ProductController {
+	return &ProductController{service}
+}
+
+func (c *ProductController) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		product, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": product,
+		})
+	}
+}
