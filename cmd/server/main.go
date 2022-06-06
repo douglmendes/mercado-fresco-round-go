6931e7f0179d7be3f9514eb069d0094eb9c7e967
h80r <h80rgbc@gmail.com>
2022-06-06 09:22:41 -0300

    Setup sectionsRoutes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+func placeholderHandle(operation string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"operation": operation})
+	}
+}
+
 func main() {
 	router := gin.Default()
+
+	sectionsRoutes := router.Group("/api/v1/sections")
+	{
+		sectionsRoutes.GET("/", placeholderHandle("get all sections"))
+		sectionsRoutes.GET("/:id", placeholderHandle("get section by id"))
+		sectionsRoutes.POST("/", placeholderHandle("create section"))
+		sectionsRoutes.PATCH("/:id", placeholderHandle("update section"))
+		sectionsRoutes.DELETE("/:id", placeholderHandle("delete section"))
+	}
 
 	router.Run()
 }
