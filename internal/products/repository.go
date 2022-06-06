82d4c1ac94d4c7e36efdeed3c032f7ab9cf4ce3c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 15:09:05 -0300

    add GetAll function to repository

--- internal/products/repository.go
+++ internal/products/repository.go
@@ -1,1 +1,26 @@
 package products
+
+import "github.com/douglmendes/mercado-fresco-round-go/pkg/store"
+
+type Repository interface {
+	GetAll() ([]Product, error)
+}
+
+type repository struct {
+	db store.Store
+}
+
+func NewRepository(db store.Store) Repository {
+	return &repository{db}
+}
+
+func (r *repository) GetAll() ([]Product, error) {
+	var products []Product
+
+	err := r.db.Read(&products)
+	if err != nil {
+		return []Product{}, err
+	}
+
+	return products, nil
+}
