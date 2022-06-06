e9a8cddde84ed7ab9505215be990eaa1d90b1e85
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 16:18:54 -0300

    add Store function to repository

--- internal/products/repository.go
+++ internal/products/repository.go
@@ -1,9 +1,12 @@
 package products
 
-import "github.com/douglmendes/mercado-fresco-round-go/pkg/store"
+import (
+	"github.com/douglmendes/mercado-fresco-round-go/pkg/store"
+)
 
 type Repository interface {
 	GetAll() ([]Product, error)
+	Store(id int, productCode, description string, width, height, length, netWeight, expirationRate, recommendedFreezingTemperature, freezingRate float64, productTypeId, sellerId int) (Product, error)
 }
 
 type repository struct {
@@ -23,4 +26,21 @@ func (r *repository) GetAll() ([]Product, error) {
 	}
 
 	return products, nil
+}
+
+func (r *repository) Store(id int, productCode, description string, width, height, length, netWeight, expirationRate, recommendedFreezingTemperature, freezingRate float64, productTypeId, sellerId int) (Product, error) {
+	var products []Product
+
+	if err := r.db.Read(&products); err != nil {
+		return Product{}, err
+	}
+
+	newProduct := Product{id, productCode, description, width, height, length, netWeight, expirationRate, recommendedFreezingTemperature, freezingRate, productTypeId, sellerId}
+	products = append(products, newProduct)
+
+	if err := r.db.Write(products); err != nil {
+		return Product{}, err
+	}
+
+	return newProduct, nil
 }
