608e18ec47f2abd4c28bd1ae2e4002d5bd18d503
h80r <h80rgbc@gmail.com>
2022-06-06 09:15:14 -0300

    Start of Requirement Sections

--- internal/sections/model.go
+++ internal/sections/model.go
@@ -1,1 +1,13 @@
 package sections
+
+type Section struct {
+	Id                 int     `json:"id,omitempty"`
+	SectionNumber      int     `json:"section_number,omitempty"`
+	CurrentTemperature float64 `json:"current_temperature,omitempty"`
+	MinimumTemperature float64 `json:"minimum_temperature,omitempty"`
+	CurrentCapacity    int     `json:"current_capacity,omitempty"`
+	MinimumCapacity    int     `json:"minimum_capacity,omitempty"`
+	MaximumCapacity    int     `json:"maximum_capacity,omitempty"`
+	WarehouseId        int     `json:"warehouse_id,omitempty"`
+	ProductTypeId      int     `json:"product_type_id,omitempty"`
+}
