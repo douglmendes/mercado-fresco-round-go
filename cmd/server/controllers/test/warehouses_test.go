57f643371997febbe389d93c95fb19a7371326a2
GustavoNKato <gustavo.nkato@gmail.com>
2022-06-19 18:54:40 -0300

    test: adding unit test getbyid controller

--- cmd/server/controllers/test/warehouses_test.go
+++ cmd/server/controllers/test/warehouses_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/douglmendes/mercado-fresco-round-go/cmd/server/controllers"
 	"github.com/douglmendes/mercado-fresco-round-go/internal/warehouses"
 	mock_warehouses "github.com/douglmendes/mercado-fresco-round-go/internal/warehouses/mock"
@@ -13,33 +14,42 @@ import (
 	"testing"
 )
 
-const relativePath = "/api/v1/warehouses/"
+const (
+	relativePath        = "/api/v1/warehouses/"
+	relativePathGetById = "/api/v1/warehouses/:id"
+	id                  = "1"
+)
 
-func TestWarehousesController_GetAll(t *testing.T) {
-	whList := []warehouses.Warehouse{
-		{
-			1,
-			"Monroe 860",
-			"47470000",
-			"TSFK",
-			10,
-			10,
-		},
-		{
-			2,
-			"Rua do Teste 2",
-			"555555555",
-			"JJJ",
-			10,
-			2,
-		},
-	}
-
+func callMock(t *testing.T) (*mock_warehouses.MockService, *controllers.WarehousesController, *gin.Engine) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	service := mock_warehouses.NewMockService(ctrl)
 	handler := controllers.NewWarehouse(service)
 	api := gin.New()
+	return service, handler, api
+}
+
+func TestWarehousesController_GetAll(t *testing.T) {
+	whList := []warehouses.Warehouse{
+		{
+			Id:                 1,
+			Address:            "Monroe 860",
+			Telephone:          "47470000",
+			WarehouseCode:      "TSFK",
+			MinimunCapacity:    10,
+			MinimunTemperature: 10,
+		},
+		{
+			Id:                 2,
+			Address:            "Rua do Teste 2",
+			Telephone:          "555555555",
+			WarehouseCode:      "JJJ",
+			MinimunCapacity:    10,
+			MinimunTemperature: 2,
+		},
+	}
+
+	service, handler, api := callMock(t)
 
 	api.GET(relativePath, handler.GetAll())
 
@@ -55,4 +65,32 @@ func TestWarehousesController_GetAll(t *testing.T) {
 	_ = json.Unmarshal(resp.Body.Bytes(), &respExpect)
 
 	assert.Equal(t, whList[0].WarehouseCode, respExpect.Data[0].WarehouseCode)
+}
+
+func TestWarehousesController_GetById(t *testing.T) {
+
+	wh := warehouses.Warehouse{
+		Id:                 1,
+		Address:            "Rua Nova",
+		Telephone:          "12121212",
+		WarehouseCode:      "GHI",
+		MinimunCapacity:    2,
+		MinimunTemperature: 2,
+	}
+
+	service, handler, api := callMock(t)
+
+	api.GET(relativePathGetById, handler.GetById())
+
+	service.EXPECT().GetById(gomock.Eq(1)).Return(wh, nil)
+
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/warehouses/%s", id), nil)
+	resp := httptest.NewRecorder()
+	api.ServeHTTP(resp, req)
+
+	respExpect := struct{ Data warehouses.Warehouse }{}
+	_ = json.Unmarshal(resp.Body.Bytes(), &respExpect)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+	assert.Equal(t, wh.WarehouseCode, respExpect.Data.WarehouseCode)
 }
