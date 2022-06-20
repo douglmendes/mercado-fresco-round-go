f28c78c696401bb4cc020a7ee232bdff6f179a0c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-20 17:20:24 -0300

    add get all ok test

--- internal/products/test/service_test.go
+++ internal/products/test/service_test.go
@@ -11,6 +11,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func callMock(t *testing.T) (*mock_products.MockRepository, products.Service) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mock_products.NewMockRepository(ctrl)
+	service := products.NewService(repository)
+
+	return repository, service
+}
+
 func TestCreate(t *testing.T) {
 	expected := products.Product{
 		Id:                             1,
@@ -148,15 +158,77 @@ func TestCreate(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			repository := mock_products.NewMockRepository(ctrl)
-			service := products.NewService(repository)
+			repository, service := callMock(t)
 
 			testCase.buildStubs(repository)
 
 			result, err := service.Create(expected)
+			testCase.checkResult(t, result, err)
+		})
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	expected := []products.Product{
+		{
+			Id:                             1,
+			ProductCode:                    "xpto",
+			Description:                    "description",
+			Width:                          6.3,
+			Height:                         2.3,
+			Length:                         5.1,
+			NetWeight:                      23.5,
+			ExpirationRate:                 0.8,
+			RecommendedFreezingTemperature: -4.3,
+			FreezingRate:                   0.4,
+			ProductTypeId:                  3,
+			SellerId:                       5,
+		},
+		{
+			Id:                             2,
+			ProductCode:                    "xablau",
+			Description:                    "description",
+			Width:                          3.6,
+			Height:                         3.2,
+			Length:                         1.5,
+			NetWeight:                      5.23,
+			ExpirationRate:                 0.08,
+			RecommendedFreezingTemperature: -3.4,
+			FreezingRate:                   0.8,
+			ProductTypeId:                  2,
+			SellerId:                       3,
+		},
+	}
+
+	testCases := []struct {
+		name        string
+		buildStubs  func(repository *mock_products.MockRepository)
+		checkResult func(t *testing.T, result []products.Product, err error)
+	}{
+		{
+			name: "OK",
+			buildStubs: func(repository *mock_products.MockRepository) {
+				repository.
+					EXPECT().
+					GetAll().
+					Times(1).
+					Return(expected, nil)
+			},
+			checkResult: func(t *testing.T, result []products.Product, err error) {
+				assert.NoError(t, err)
+
+				assert.ElementsMatch(t, expected, result)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repository, service := callMock(t)
+
+			testCase.buildStubs(repository)
+
+			result, err := service.GetAll()
 			testCase.checkResult(t, result, err)
 		})
 	}
