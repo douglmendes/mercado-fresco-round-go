cb9163c6beaa7a9498557ba35483f5f1b0b90256
GustavoNKato <gustavo.nkato@gmail.com>
2022-06-29 00:09:24 -0300

    feat: adding context and locality at domain, removing fields useless

--- internal/warehouses/domain/warehouse.go
+++ internal/warehouses/domain/warehouse.go
@@ -1,28 +1,29 @@
 package domain
 
+import "context"
+
 type Warehouse struct {
-	Id                 int    `json:"id"`
-	Address            string `json:"address"`
-	Telephone          string `json:"telephone"`
-	WarehouseCode      string `json:"warehouse_code"`
-	MinimunCapacity    int    `json:"minimun_capacity"`
-	MinimunTemperature int    `json:"minimun_temperature"`
+	Id            int64  `json:"id"`
+	Address       string `json:"address"`
+	Telephone     string `json:"telephone"`
+	WarehouseCode string `json:"warehouse_code"`
+	LocalityId    int64  `json:"locality_id"`
 }
 
 //go:generate mockgen -source=./warehouse.go -destination=./mock/warehouse_mock.go
 type WarehouseService interface {
-	Create(address, telephone, warehouseCode string, minimunCapacity, minimunTemperature int) (*Warehouse, error)
-	GetAll() ([]Warehouse, error)
-	GetById(id int) (Warehouse, error)
-	Update(id int, address, telephone, warehouseCode string, minimunCapacity, minimunTemperature int) (Warehouse, error)
-	Delete(id int) error
+	Create(ctx context.Context, address, telephone, warehouseCode string, localityId int64) (*Warehouse, error)
+	GetAll(ctx context.Context) ([]Warehouse, error)
+	GetById(ctx context.Context, id int64) (Warehouse, error)
+	Update(ctx context.Context, id int64, address, telephone, warehouseCode string, localityId int64) (Warehouse, error)
+	Delete(ctx context.Context, id int64) error
 }
 
 type WarehouseRepository interface {
-	Create(id int, address, telephone, warehouseCode string, minimunCapacity, minimunTemperature int) (Warehouse, error)
-	LastID() (int, error)
-	GetAll() ([]Warehouse, error)
-	GetById(id int) (Warehouse, error)
-	Update(id int, address, telephone, warehouseCode string, minimunCapacity, minimunTemperature int) (Warehouse, error)
-	Delete(id int) error
+	Create(ctx context.Context, address, telephone, warehouseCode string, localityId int64) (Warehouse, error)
+	LastID() (int64, error)
+	GetAll(ctx context.Context) ([]Warehouse, error)
+	GetById(ctx context.Context, id int64) (Warehouse, error)
+	Update(ctx context.Context, id int64, address, telephone, warehouseCode string, localityId int64) (Warehouse, error)
+	Delete(ctx context.Context, id int64) error
 }
