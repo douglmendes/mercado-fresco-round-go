f83eed98115940fbee0f0416c943cba0a3165f93
GustavoNKato <gustavo.nkato@gmail.com>
2022-07-05 16:45:07 -0300

    test: adding mock context

--- internal/warehouses/controller/warehouse_controller_test.go
+++ internal/warehouses/controller/warehouse_controller_test.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -24,7 +23,7 @@ const (
 	locality           = 101
 )
 
-var ctx = context.TODO()
+var ctxMock = gomock.Any()
 
 func callWarehousesMock(t *testing.T) (*mockwarehouses.MockWarehouseService, *WarehousesController, *gin.Engine) {
 	ctrl := gomock.NewController(t)
@@ -52,13 +51,13 @@ func TestWarehousesController_GetAll(t *testing.T) {
 	}
 
 	service, handler, api := callWarehousesMock(t)
+	req := httptest.NewRequest(http.MethodGet, relativePath, nil)
+	resp := httptest.NewRecorder()
 
 	api.GET(relativePath, handler.GetAll())
 
-	service.EXPECT().GetAll().Return(whList, nil)
+	service.EXPECT().GetAll(ctxMock).Return(whList, nil)
 
-	req := httptest.NewRequest(http.MethodGet, relativePath, nil)
-	resp := httptest.NewRecorder()
 	api.ServeHTTP(resp, req)
 
 	assert.Equal(t, http.StatusOK, resp.Code)
@@ -74,7 +73,7 @@ func TestWarehousesController_GetAll_NOK(t *testing.T) {
 
 	api.GET(relativePath, handler.GetAll())
 
-	service.EXPECT().GetAll(context.Background()).Return([]domain.Warehouse{}, errors.New("error 404"))
+	service.EXPECT().GetAll(ctxMock).Return([]domain.Warehouse{}, errors.New("error 404"))
 
 	req := httptest.NewRequest(http.MethodGet, relativePath, nil)
 	resp := httptest.NewRecorder()
@@ -98,7 +97,7 @@ func TestWarehousesController_GetById(t *testing.T) {
 
 	api.GET(relativePathWithId, handler.GetById())
 
-	service.EXPECT().GetById(ctx, gomock.Eq(idNumber)).Return(wh, nil)
+	service.EXPECT().GetById(ctxMock, gomock.Eq(idNumber)).Return(wh, nil)
 
 	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/warehouses/%s", idString), nil)
 	resp := httptest.NewRecorder()
@@ -114,7 +113,7 @@ func TestWarehousesController_GetById(t *testing.T) {
 func TestWarehousesController_GetById_NOK(t *testing.T) {
 	service, handler, api := callWarehousesMock(t)
 	api.GET(relativePathWithId, handler.GetById())
-	service.EXPECT().GetById(ctx, gomock.Eq(idNumber)).Return(domain.Warehouse{}, errors.New("warehouse not found"))
+	service.EXPECT().GetById(ctxMock, gomock.Eq(idNumber)).Return(domain.Warehouse{}, errors.New("warehouse not found"))
 
 	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/warehouses/%s", idString), nil)
 	resp := httptest.NewRecorder()
@@ -147,7 +146,7 @@ func TestWarehousesController_Create(t *testing.T) {
 	api.POST(relativePath, handler.Create())
 
 	service.EXPECT().Create(
-		ctx,
+		ctxMock,
 		"Rua 1",
 		"555555555",
 		"ZAQ",
@@ -167,7 +166,7 @@ func TestWarehousesController_Create_Conflict(t *testing.T) {
 	api.POST(relativePath, handler.Create())
 
 	service.EXPECT().Create(
-		ctx,
+		ctxMock,
 		"Rua 1",
 		"555555555",
 		"ZAQ",
@@ -198,7 +197,7 @@ func TestWarehousesController_Delete_OK(t *testing.T) {
 	service, handler, api := callWarehousesMock(t)
 	api.DELETE(relativePathWithId, handler.Delete())
 
-	service.EXPECT().Delete(ctx, gomock.Eq(idNumber)).Return(nil)
+	service.EXPECT().Delete(ctxMock, gomock.Eq(idNumber)).Return(nil)
 
 	req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/warehouses/%s", idString), nil)
 	resp := httptest.NewRecorder()
@@ -211,7 +210,7 @@ func TestWarehousesController_Delete_NOK(t *testing.T) {
 	service, handler, api := callWarehousesMock(t)
 	api.DELETE(relativePathWithId, handler.Delete())
 
-	service.EXPECT().Delete(ctx, gomock.Eq(idNumber)).Return(errors.New("erro 404"))
+	service.EXPECT().Delete(ctxMock, gomock.Eq(idNumber)).Return(errors.New("erro 404"))
 
 	req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/warehouses/%s", idString), nil)
 	resp := httptest.NewRecorder()
@@ -244,7 +243,7 @@ func TestWarehousesController_Update(t *testing.T) {
 	api.PATCH(relativePathWithId, handler.Update())
 
 	service.EXPECT().Update(
-		ctx,
+		ctxMock,
 		idNumber,
 		"Rua Sem Saida",
 		"888888888",
@@ -271,7 +270,7 @@ func TestWarehousesController_Update_NOK(t *testing.T) {
 	api.PATCH(relativePathWithId, handler.Update())
 
 	service.EXPECT().Update(
-		ctx,
+		ctxMock,
 		idNumber,
 		"Rua Sem Saida",
 		"888888888",
