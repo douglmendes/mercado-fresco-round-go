ea9f4e3c24bb3e76ad68d1200684165165582c43
GustavoNKato <gustavo.nkato@gmail.com>
2022-07-09 16:57:00 -0300

    feat: implementing the called to repository for get a carrier report

--- internal/localities/service/service.go
+++ internal/localities/service/service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-
 	"github.com/douglmendes/mercado-fresco-round-go/internal/localities/domain"
 )
 
@@ -16,14 +15,6 @@ func NewService(r domain.LocalityRepository) domain.LocalityService {
 	}
 }
 
-// func (s service) GetAll(ctx context.Context) ([]domain.Locality, error) {
-// 	lc, err := s.repository.GetAll(ctx)
-// 	if err != nil {
-// 		return []domain.Locality{}, err
-// 	}
-// 	return lc, nil
-// }
-
 func (s service) GetById(ctx context.Context, id int) (domain.Locality, error) {
 	lc, err := s.repository.GetById(ctx, id)
 	if err != nil {
@@ -32,12 +23,29 @@ func (s service) GetById(ctx context.Context, id int) (domain.Locality, error) {
 	return lc, nil
 }
 
+// func (s service) GetAll(ctx context.Context) ([]domain.Locality, error) {
+// 	lc, err := s.repository.GetAll(ctx)
+// 	if err != nil {
+// 		return []domain.Locality{}, err
+// 	}
+// 	return lc, nil
+// }
+
 func (s service) GetBySellers(ctx context.Context, id int) ([]domain.SellersByLocality, error) {
 	lc, err := s.repository.GetBySellers(ctx, id)
 	if err != nil {
 		return []domain.SellersByLocality{}, err
 	}
 	return lc, nil
+}
+
+func (s service) GetByCarriers(ctx context.Context, id int) ([]domain.CarriersByLocality, error) {
+	carrier, err := s.repository.GetByCarriers(ctx, id)
+	if err != nil {
+		return []domain.CarriersByLocality{}, err
+	}
+
+	return carrier, nil
 }
 
 func (s service) Create(ctx context.Context, localityname, provinceName, countryName string) (domain.Locality, error) {
