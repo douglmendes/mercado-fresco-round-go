a9f7baded801ebd8bb1429657d6bb6bfd59e737b
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-07-09 15:23:14 -0300

    add products routes GET /reportRecords

--- cmd/server/routes/products_routes.go
+++ cmd/server/routes/products_routes.go
@@ -2,6 +2,9 @@ package routes
 
 import (
 	"github.com/douglmendes/mercado-fresco-round-go/connections"
+	productRecordController "github.com/douglmendes/mercado-fresco-round-go/internal/product_record/controller"
+	productRecordMariaDB "github.com/douglmendes/mercado-fresco-round-go/internal/product_record/repository/mariadb"
+	productRecordService "github.com/douglmendes/mercado-fresco-round-go/internal/product_record/service"
 	"github.com/douglmendes/mercado-fresco-round-go/internal/products/controller"
 	"github.com/douglmendes/mercado-fresco-round-go/internal/products/repository/mariadb"
 	"github.com/douglmendes/mercado-fresco-round-go/internal/products/service"
@@ -11,9 +14,15 @@ import (
 func ProductsRoutes(group *gin.RouterGroup) {
 	productRouterGroup := group.Group("/products")
 	{
-		productsRepository := mariadb.NewRepository(connections.NewConnection())
+		connection := connections.NewConnection()
+
+		productsRepository := mariadb.NewRepository(connection)
 		productsService := service.NewService(productsRepository)
 		productsController := controller.NewProductController(productsService)
+
+		productRecordsRepository := productRecordMariaDB.NewRepository(connection)
+		productRecordsService := productRecordService.NewProductRecordService(productRecordsRepository, productsRepository)
+		productRecordController := productRecordController.NewProductRecordController(productRecordsService)
 
 		productRouterGroup.POST("/", productsController.Create())
 		productRouterGroup.GET("/", productsController.GetAll())
@@ -21,5 +30,6 @@ func ProductsRoutes(group *gin.RouterGroup) {
 		productRouterGroup.PATCH("/:id", productsController.Update())
 		productRouterGroup.DELETE("/:id", productsController.Delete())
 
+		productRouterGroup.GET("/reportRecords", productRecordController.GetByProductId())
 	}
 }
