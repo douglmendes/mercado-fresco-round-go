16f0012db5774a18710359f2bdc06f0f8eff94da
h80r <h80rgbc@gmail.com>
2022-07-12 13:44:00 -0300

    Start of Requirement Product Batches

---
+++ internal/product_batches/controller/controller.go
@@ -0,0 +1,99 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/douglmendes/mercado-fresco-round-go/internal/product_batches/domain"
+	"github.com/douglmendes/mercado-fresco-round-go/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ProductBatchesController struct {
+	service domain.ProductBatchesService
+}
+
+func NewController(pbs domain.ProductBatchesService) *ProductBatchesController {
+	return &ProductBatchesController{
+		service: pbs,
+	}
+}
+
+// Create godoc
+// @Summary Create product batches
+// @Tags Products
+// @Description create a product batch
+// @Accept  json
+// @Produce  json
+// @Param product_batch body createRequest true "ProductBatch to create"
+// @Success 201 {object} domain.ProductBatch
+// @Failure 422 {object} response.Response
+// @Router /api/v1/productBatches [post]
+func (c *ProductBatchesController) Create() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var request createRequest
+
+		if err := ctx.ShouldBindJSON(&request); err != nil {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return
+		}
+
+		productBatch, err := c.service.Create(
+			ctx,
+			request.BatchNumber,
+			request.CurrentQuantity,
+			request.CurrentTemperature,
+			request.DueDate,
+			request.InitialQuantity,
+			request.ManufacturingDate,
+			request.ManufacturingHour,
+			request.MinimumTemperature,
+			request.ProductId,
+			request.SectionId,
+		)
+
+		if err != nil {
+			ctx.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, productBatch)
+	}
+}
+
+func (c *ProductBatchesController) GetBySectionId() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var id int64
+		var err error
+
+		if stringId, exists := ctx.GetQuery("id"); exists {
+			id, err = strconv.ParseInt(stringId, 10, 64)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, response.DecodeError(err.Error()))
+				return
+			}
+		}
+
+		records, err := c.service.GetBySectionId(ctx, int(id))
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, response.NewResponse(records))
+	}
+}
+
+type createRequest struct {
+	BatchNumber        int    `json:"batch_number,omitempty"`
+	CurrentQuantity    int    `json:"current_quantity,omitempty"`
+	CurrentTemperature int    `json:"current_temperature,omitempty"`
+	DueDate            string `json:"due_date,omitempty"`
+	InitialQuantity    int    `json:"initial_quantity,omitempty"`
+	ManufacturingDate  string `json:"manufacturing_date,omitempty"`
+	ManufacturingHour  int    `json:"manufacturing_hour,omitempty"`
+	MinimumTemperature int    `json:"minimum_temperature,omitempty"`
+	ProductId          int    `json:"product_id,omitempty"`
+	SectionId          int    `json:"section_id,omitempty"`
+}
---
+++ internal/product_batches/service/service.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	pbRepo "github.com/douglmendes/mercado-fresco-round-go/internal/product_batches/domain"
+	productRepo "github.com/douglmendes/mercado-fresco-round-go/internal/products/domain"
+	sectionRepo "github.com/douglmendes/mercado-fresco-round-go/internal/sections/domain"
+)
+
+type service struct {
+	productBatchesRepository pbRepo.ProductBatchesRepository
+	productRepo              productRepo.ProductRepository
+	sectionRepo              sectionRepo.Repository
+}
+
+func NewService(pbr pbRepo.ProductBatchesRepository, pr productRepo.ProductRepository, sr sectionRepo.Repository) pbRepo.ProductBatchesService {
+	return &service{
+		productBatchesRepository: pbr,
+		productRepo:              pr,
+		sectionRepo:              sr,
+	}
+}
+
+func (s *service) Create(ctx context.Context, batchNumber, currentQuantity, currentTemperature int, dueDate string, initialQuantity int, manufacturingDate string, manufacturingHour, minimumTemperature, productId, sectionId int) (*pbRepo.ProductBatch, error) {
+	productBatches, err := s.productBatchesRepository.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, productBatch := range productBatches {
+		if productBatch.BatchNumber == batchNumber {
+			return nil, fmt.Errorf("a product batch with the batch_number %d already exists", batchNumber)
+		}
+	}
+
+	product, err := s.productRepo.GetById(productId)
+	if product.Id == 0 || err != nil {
+		return nil, fmt.Errorf("product %d not found", productId)
+	}
+
+	section, err := s.sectionRepo.GetById(sectionId)
+	if section.Id == 0 || section == nil || err != nil {
+		return nil, fmt.Errorf("section %d not found", productId)
+	}
+
+	return s.productBatchesRepository.Create(ctx, batchNumber, currentQuantity, currentTemperature, dueDate, initialQuantity, manufacturingDate, manufacturingHour, minimumTemperature, productId, sectionId)
+}
+
+func (s *service) GetBySectionId(ctx context.Context, sectionId int) ([]pbRepo.SectionRecords, error) {
+	if sectionId != 0 {
+		_, err := s.sectionRepo.GetById(sectionId)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	records, err := s.productBatchesRepository.GetBySectionId(ctx, sectionId)
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
