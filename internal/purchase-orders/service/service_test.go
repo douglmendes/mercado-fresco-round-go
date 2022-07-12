4450a941970b361220d8b80a75d7eb745012f90a
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-07-12 11:00:14 -0300

    add purchase orders service Create OK test

---
+++ internal/purchase-orders/service/service_test.go
@@ -0,0 +1,102 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/douglmendes/mercado-fresco-round-go/internal/purchase-orders/domain"
+	mock_domain "github.com/douglmendes/mercado-fresco-round-go/internal/purchase-orders/domain/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	ONCE = 1
+)
+
+var (
+	purchaseOrder = domain.PurchaseOrder{
+		Id:              1,
+		OrderNumber:     "xpto",
+		OrderDate:       "2020-02-02",
+		TrackingCode:    "ew23143543jn",
+		BuyerId:         1,
+		ProductRecordId: 1,
+		OrderStatusId:   1,
+	}
+	noPurchaseOrders = []domain.PurchaseOrder{}
+)
+
+func callMock(t *testing.T) (
+	*mock_domain.MockRepository,
+	domain.Service,
+	context.Context,
+) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mock_domain.NewMockRepository(ctrl)
+	service := NewService(repository)
+
+	return repository, service, context.Background()
+}
+
+func TestCreate(t *testing.T) {
+	testCases := []struct {
+		name          string
+		buildStubs    func(repository *mock_domain.MockRepository, ctx context.Context)
+		purchaseOrder domain.PurchaseOrder
+		checkResult   func(t *testing.T, result *domain.PurchaseOrder, err error)
+	}{
+		{
+			name: "OK",
+			buildStubs: func(repository *mock_domain.MockRepository, ctx context.Context) {
+				repository.
+					EXPECT().
+					GetAll(ctx).
+					Times(ONCE).
+					Return(noPurchaseOrders, nil)
+
+				repository.
+					EXPECT().
+					Create(
+						ctx,
+						purchaseOrder.OrderNumber,
+						purchaseOrder.OrderDate,
+						purchaseOrder.TrackingCode,
+						purchaseOrder.BuyerId,
+						purchaseOrder.ProductRecordId,
+						purchaseOrder.OrderStatusId,
+					).
+					Times(ONCE).
+					Return(&purchaseOrder, nil)
+			},
+			purchaseOrder: purchaseOrder,
+			checkResult: func(t *testing.T, result *domain.PurchaseOrder, err error) {
+				assert.NoError(t, err)
+
+				assert.Equal(t, &purchaseOrder, result)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repository, service, ctx := callMock(t)
+
+			testCase.buildStubs(repository, ctx)
+
+			result, err := service.Create(
+				context.Background(),
+				testCase.purchaseOrder.OrderNumber,
+				testCase.purchaseOrder.OrderDate,
+				testCase.purchaseOrder.TrackingCode,
+				testCase.purchaseOrder.BuyerId,
+				testCase.purchaseOrder.ProductRecordId,
+				testCase.purchaseOrder.OrderStatusId,
+			)
+
+			testCase.checkResult(t, result, err)
+		})
+	}
+}
