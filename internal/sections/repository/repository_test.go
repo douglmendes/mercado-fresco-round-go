3ab12e674b23f21ec932cd25981a740b01c0f8d0
h80r <h80rgbc@gmail.com>
2022-07-12 16:08:49 -0300

    Initialize repository testing

---
+++ internal/sections/repository/repository_test.go
@@ -0,0 +1,23 @@
+package repository
+
+import "testing"
+
+func TestRepository_Create(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestRepository_Get_All(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestRepository_Get_ById(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestRepository_Update(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestRepository_Delete(t *testing.T) {
+	t.Skip("Not implemented")
+}
