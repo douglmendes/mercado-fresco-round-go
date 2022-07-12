a28d86201f1d3b106507ff6c0afd4757024fb56f
GustavoNKato <gustavo.nkato@gmail.com>
2022-07-11 22:57:07 -0300

    test: carriers controller crate conflict

--- internal/carriers/controller/carrier_controller_test.go
+++ internal/carriers/controller/carrier_controller_test.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"bytes"
+	"errors"
 	"github.com/douglmendes/mercado-fresco-round-go/internal/carriers/domain"
 	mockcarriers "github.com/douglmendes/mercado-fresco-round-go/internal/carriers/domain/mock"
 	"github.com/gin-gonic/gin"
@@ -67,4 +68,26 @@ func TestCarrierController_Create_StatusUnprocessableEntity(t *testing.T) {
 	api.ServeHTTP(resp, req)
 
 	assert.Equal(t, http.StatusUnprocessableEntity, resp.Code)
+}
+
+func TestCarrierController_Create_Conflict(t *testing.T) {
+	service, handler, api := callCarriersMock(t)
+	api.POST(carriersRelativePath, handler.Create())
+
+	service.EXPECT().CreateCarrier(
+		ctxMock,
+		"DUDE",
+		"Other Company",
+		"Rua dos outros amigos",
+		"1133111111",
+		1,
+	).Return(domain.Carrier{}, errors.New("already exists a carrier with this cid: DUDE"))
+
+	payload := `{"cid": "DUDE", "company_name": "Other Company", "address": "Rua dos outros amigos", "telephone": "1133111111", "locality_id": 1}`
+	req := httptest.NewRequest(http.MethodPost, carriersRelativePath, bytes.NewBuffer([]byte(payload)))
+	resp := httptest.NewRecorder()
+	api.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusConflict, resp.Code)
+
 }
