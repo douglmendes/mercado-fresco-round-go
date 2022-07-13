02233da3573739666413ffc001bcbeaa4c8c1fee
h80r <h80rgbc@gmail.com>
2022-07-13 08:57:46 -0300

    Export repository queries into single file

--- internal/product_batches/repository/repository.go
+++ internal/product_batches/repository/repository.go
@@ -7,13 +7,6 @@ import (
 	"github.com/douglmendes/mercado-fresco-round-go/internal/product_batches/domain"
 )
 
-const (
-	createQuery              = "INSERT INTO product_batches (batch_number, current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_hour, minimum_temperature, product_id, section_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-	getQuery                 = "SELECT id, batch_number, current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_hour, minimum_temperature, product_id, section_id FROM product_batches"
-	singleSectionReportQuery = "SELECT product_batches.current_quantity, sections.section_id, sections.section_number FROM product_batches INNER JOIN sections ON product_batches.section_id = sections.id WHERE product_batches.section_id = ?"
-	allSectionsReportQuery   = "SELECT product_batches.current_quantity, sections.section_id, sections.section_number FROM product_batches INNER JOIN sections ON product_batches.section_id = sections.id"
-)
-
 type repository struct {
 	db *sql.DB
 }
